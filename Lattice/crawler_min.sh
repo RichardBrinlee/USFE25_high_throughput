fd03ea544f5ac79d4be838f15640856dae7eab27
RichardBrinlee <163084428+RichardBrinlee@users.noreply.github.com>
2024-11-24 01:34:19 -0600

    Update crawler_min.sh

--- Lattice/crawler_min.sh
+++ Lattice/crawler_min.sh
@@ -4,8 +4,16 @@ i=1
 cd 1
 while [ $i -le 99 ]
 do
+    # Run the min.sh script in each directory
     sh min.sh
-    tail min_aE >> ../sat.dat
+
+    # fine the name of the directory
+    dir=$(basename `pwd`)
+
+    # Extract the second column from min_aE and append it to lattice.txt
+    awk '{print $2}' min_aE >> ../$dir.txt
+
+    # add the i
     ((i++))
     cd ../$i
 done
